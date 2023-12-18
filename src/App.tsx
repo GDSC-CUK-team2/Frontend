884aff0abd863a3975fc05ae0be8470eb8881197
ys0222 <ggdbstjd0222@gmail.com>
2023-12-19 01:32:03 +0900

    Chore : 빌드 관련 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import Main from './pages/Main';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import {Route,Routes } from 'react-router-dom';
 import Search from './pages/SearchResult';
 
 // api테스하는 페이지
 import Test from './pages/Test';
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
 
     <Routes>
       <Route path='/' element={<Main />} />
@@ -16,7 +15,6 @@ function App() {
 
       <Route path='/test' element={<Test />} />
     </Routes>
-    </BrowserRouter>
   );
 }
 
